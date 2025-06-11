83b2627aaca901c936eba70af0da4b3eee438431
ybh125 <hoferecht.yannik@gmail.com>
2025-06-11 23:13:17 +0200

    Add roomtour files

---
+++ roomtour/data.js
@@ -0,0 +1,445 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fahrrad",
+      "name": "fahrrad",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.3586266200840784,
+        "pitch": 0.16231050503414046,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.724055368220448,
+          "pitch": 0.3747511802023311,
+          "rotation": 0,
+          "target": "7-studio-eingang"
+        },
+        {
+          "yaw": 1.657239833296602,
+          "pitch": 0.1710435578129843,
+          "rotation": 0,
+          "target": "8-bar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-durchgang",
+      "name": "durchgang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.005208851055328978,
+        "pitch": 0.20561565248886993,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.694403372603528,
+          "pitch": 0.3435148331818958,
+          "rotation": 0,
+          "target": "9-sofas"
+        },
+        {
+          "yaw": 0.052127819885729565,
+          "pitch": 0.2725447308215454,
+          "rotation": 0,
+          "target": "2-dancefloor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dancefloor",
+      "name": "dancefloor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.010750080113842486,
+        "pitch": 0.14059004110004736,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.004263102484912906,
+          "pitch": 0.37425675687270754,
+          "rotation": 0,
+          "target": "3-stage"
+        },
+        {
+          "yaw": -2.9729154059744545,
+          "pitch": 0.3717868162655922,
+          "rotation": 0,
+          "target": "1-durchgang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-stage",
+      "name": "stage",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.3374817281058622,
+        "pitch": 0.2254709671660784,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6763314975763723,
+          "pitch": 0.12208985111581683,
+          "rotation": 0,
+          "target": "4-klo"
+        },
+        {
+          "yaw": -1.2907683103173913,
+          "pitch": 0.15533079894004587,
+          "rotation": 0,
+          "target": "4-klo"
+        },
+        {
+          "yaw": 3.0979100328919227,
+          "pitch": 0.5571004585555848,
+          "rotation": 0,
+          "target": "2-dancefloor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-klo",
+      "name": "klo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.375545794038466,
+        "pitch": 0.36274074155050684,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.544760590009595,
+          "pitch": 0.3157744085414258,
+          "rotation": 0,
+          "target": "3-stage"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-studio-werkstatt",
+      "name": "studio werkstatt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.003011226825304,
+        "pitch": 0.22376285377847793,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.035925265433693454,
+          "pitch": 0.17987711003753404,
+          "rotation": 0,
+          "target": "6-studio-bro"
+        },
+        {
+          "yaw": 0.6636894730191756,
+          "pitch": 0.33824707145985755,
+          "rotation": 0,
+          "target": "7-studio-eingang"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-studio-bro",
+      "name": "studio büro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.013425672039250713,
+        "pitch": 0.2539961273408551,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1014273386858395,
+          "pitch": 0.26547172714496625,
+          "rotation": 0,
+          "target": "5-studio-werkstatt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-studio-eingang",
+      "name": "studio eingang",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.06805076757850159,
+        "pitch": 0.19513901041037585,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.07614326200103605,
+          "pitch": 0.1698627009751963,
+          "rotation": 0,
+          "target": "5-studio-werkstatt"
+        },
+        {
+          "yaw": -2.8482425937797977,
+          "pitch": 0.5081876040030657,
+          "rotation": 0,
+          "target": "0-fahrrad"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-bar",
+      "name": "bar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.100158568641344,
+        "pitch": 0.2307841292444195,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.1972355934277754,
+          "pitch": 0.20336223574621215,
+          "rotation": 0,
+          "target": "0-fahrrad"
+        },
+        {
+          "yaw": -1.7325747715043907,
+          "pitch": 0.3283673063672232,
+          "rotation": 0,
+          "target": "9-sofas"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sofas",
+      "name": "sofas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.6958197766925611,
+        "pitch": 0.33646280777541726,
+        "fov": 1.3965535682726662
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7764162426308303,
+          "pitch": 0.261907802568647,
+          "rotation": 0,
+          "target": "8-bar"
+        },
+        {
+          "yaw": -1.3552529498684933,
+          "pitch": 0.3454292930012919,
+          "rotation": 0,
+          "target": "1-durchgang"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "GARAGE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
